533ad88c2c1420a9e48305246737fb058e57e317
khaleghi78 <khaleghirt@gmail.com>
2023-01-04 20:08:58 +0330

    ...

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Navbar.scss'
 import { Link } from 'react-router-dom'
+import { setSidebarOn } from '../../store/sidebarSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllCategories } from '../../store/categorySlice'
 
 const Navbar = () => {
+const dispatch = useDispatch()
+const categories = useSelector(getAllCategories)
+
   return (
     <nav className='navbar'>
       <div className='navbar-cnt flex align-center'>
         <div className='brand-and-toggler flex align-center'>
-          <button type='button' className='sidebar-show-btn text-white'>
+          <button type='button' className='sidebar-show-btn text-white'
+           onClick={() => dispatch(setSidebarOn())}>
           <i className='fas fa-bars'></i>
           </button>
           <Link to="/" className='navbar-brand flex align-center'>
@@ -33,9 +40,13 @@ const Navbar = () => {
           </div>
 
           <ul className='navbar-nav flex align-center fs-12 fw-4 font-manrope'>
-            <li className='nav-item no-wrap'>
-              <Link to="" className='nav-link text-capitalize'>Category here</Link>
-            </li>
+            {
+              categories.slice(0, 8).map((category, idx) => (
+                <li className='nav-item no-wrap' key={idx}>
+                <Link to={`category/${category}`} className='nav-link text-capitalize'>{category}</Link>
+                </li>
+              ))
+            }
           </ul>
         </div>
 
