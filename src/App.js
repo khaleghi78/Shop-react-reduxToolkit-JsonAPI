945e0f91eb850a98728033488e03c39981c5f5e6
khaleghi78 <khaleghirt@gmail.com>
2023-01-04 12:21:39 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     hello
+    </div>
+  );
+}
+
+export default App;
